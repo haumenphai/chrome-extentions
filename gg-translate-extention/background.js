0f315a67df0bee620467bdd194eb456bc8e96559
haumenphai <codegunpow@gmail.com>
2021-07-02 16:03:13 +0700

    up up

--- gg-translate-extention/background.js
+++ gg-translate-extention/background.js
@@ -1,23 +1,33 @@
 chrome.runtime.onInstalled.addListener(() => {
     chrome.contextMenus.create({
         id: 'upupup123',
-        title: 'Translate A-V',
+        title: 'Translate in new window',
         type: 'normal',
         contexts: ['selection'],
     })
 })
 
+var left = 500
+chrome.windows.getCurrent((window) => left = window.width / 4)
+
+var langIn = 'auto'
+var langOut = 'vi'
+
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
     console.log('click context menu')
-    chrome.storage.local.get(['window_id'], (values) => chrome.windows.remove(values.window_id))  
+    chrome.storage.local.get(['window_id'], (values) => chrome.windows.remove(values.window_id))
 
-    langin = 'auto'
-    langout = 'vi'
+    chrome.storage.local.get(['langIn', 'langOut'], (values) => {
+        langIn = values.langIn
+        langOut = values.langOut
 
-    chrome.windows.create({
-        url: 'https://translate.google.com/?hl='+langout+'&sl='+langin+'&tl='+langout+'&text=' + info.selectionText + '&op=translate',
-        focused: true,
-        height: 500 
-    }, (window) => chrome.storage.local.set({'window_id': window.id}))
+        chrome.windows.create({
+            url: 'https://translate.google.com/?hl=' + langOut + '&sl=' + langIn + '&tl=' + langOut + '&text=' + info.selectionText + '&op=translate',
+            focused: true,
+            height: 500,
+            width: 1000,
+            left: left
+        }, (window) => chrome.storage.local.set({ 'window_id': window.id }))
+    });  
 })
---
+++ gg-translate-extention/js/popup.js
@@ -0,0 +1,14 @@
+let btnSaveSetting = document.getElementById("btnSaveSetting")
+let langInSelect = document.getElementById("langIn")
+let langOutSelect = document.getElementById("langOut")
+
+chrome.storage.local.get(['langIn', 'langOut'], (values) => {
+    langInSelect.value = values.langIn
+    langOutSelect.value = values.langOut
+
+});
+
+btnSaveSetting.addEventListener("click", async () => {
+    btnSaveSetting.innerText = langInSelect.value + ' - ' + langOutSelect.value
+    chrome.storage.local.set({'langIn': langInSelect.value, 'langOut': langOutSelect.value}, () => {});
+})
